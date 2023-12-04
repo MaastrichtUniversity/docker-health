bc6399d2d1f04c28e1fb03dcc9401a9fc1863fa0
hkabbech <helene.kabbech@gmail.com>
2023-12-04 16:39:14 +0100

    cleaning and post flat composition

--- flatehr/ETL-flatehr.py
+++ flatehr/ETL-flatehr.py
@@ -5,12 +5,13 @@ from contextlib import redirect_stdout
 import io
 import json
 import yaml
+import ast
 from flatehr.cli import generate
 from flatehr.cli.inspect_template import main as inspect
 
 from src.template import fetch_all_templates, post_template
 from src.ehr import create_ehr, fetch_all_ehr_id
-from src.composition import post_composition
+from src.composition import post_flat_composition
 
 TEMPLATE_PATH = Path("data/templates")
 SYNTHEA_PATH = Path("data/synthea/json")
@@ -22,7 +23,8 @@ conf_skfile = CONF_PATH / "patient_skeleton_conf.yaml"
 conf_file = CONF_PATH / "patient_conf.yaml"
 
 
-print(f"STEP 1: Generate a skeleton configuration yaml file: {conf_skfile}")
+print(f"\nSTEP 1: Generate a skeleton configuration yaml file: {conf_skfile}")
+# This step is only used to generated a skeleton of the configuration file from the web template
 
 skeleton_conf = io.StringIO()
 with redirect_stdout(skeleton_conf):
@@ -30,40 +32,25 @@ with redirect_stdout(skeleton_conf):
 with open(conf_skfile, "w") as file:
     print(skeleton_conf.getvalue(), file=file)
 
-# with open(conf_skfile, "r") as file:
-#     conf_dict = yaml.safe_load(file)
-#     assert type(conf_dict) == dict
+with open(conf_skfile, "r") as file:
+    print(file.read())
+    # conf_dict = yaml.safe_load(file)
+    # assert type(conf_dict) == dict
 
 
-print(f"STEP 2: Fill the skeleton configuration yaml file based on template/data: {conf_file}")
-# https://jsonpath.com/
+print(f"\nSTEP 2: Fill the skeleton configuration yaml file based on template/data: {conf_file}")
+# A new dict is created with the required parameters described in the skeleton conf file
+# The dict is filled with the fixed parameters and all paths should be written following JsonPath Syntax: https://jsonpath.com/
+# These paths extract the data source and store them to a flat composition
+# The dict is then dumped to a yaml file
 
+# generate a random ehr identifier if necessary (can be off using the option skip_ehr_id=True in generate.from_file)
 conf_dict = {
     "ehr_id": {"maps_to": [], "value": "{{ random_ehr_id() }}"},
     "paths": {}
 }
 
-conf_dict["paths"]["ctx/category|code"] = "433|event|"
-  # other codes:
-  # 431|persistent| - of potential life-time validity;
-  # 451|episodic| - valid over the life of a care episode;
-  # 433|event| - valid at the time of recording (long-term validity requires subsequent clinical assessment).
-
-conf_dict["paths"]["ctx/composer|name"] = "DataHub"
-# conf_dict["paths"]["ctx/composer|id"] = ??
-# conf_dict["paths"]["ctx/composer|id_scheme"] =??
-# conf_dict["paths"]["ctx/composer|id_namespace"] = ??
-
-# conf_dict["paths"]["ctx/setting|code"] = ??
-# conf_dict["paths"]["ctx/setting|value"] = ??
-
-conf_dict["paths"]["ctx/encoding|code"] = "UTF-8"
-conf_dict["paths"]["ctx/language"] = "en"
-conf_dict["paths"]["ctx/territory"] = "nl"
-
-
-conf_dict["paths"]["ctx/start_time"] = datetime.now().isoformat()
-
+# parameters linked to the data source:
 conf_dict["paths"]["ctx/subject"] = {
     "maps_to": ["$.attributes.id", "$.attributes.first_name", "$.attributes.last_name"],
     "suffixes": {"|id": "{{ maps_to[0] }}", "|first_name": "{{ maps_to[1] }}", "|last_name": "{{ maps_to[2] }}"}
@@ -84,15 +71,44 @@ conf_dict["paths"]["patient/gender/sex_assigned_at_birth"] = {
     "suffixes": {"|code": "{{ maps_to[0] }}"}
 }
 
+# "fixed" hyper parameters:
+conf_dict["paths"]["ctx/category|code"] = "433"
+  # other codes:
+  # 431|persistent| - of potential life-time validity;
+  # 451|episodic| - valid over the life of a care episode;
+  # 433|event| - valid at the time of recording (long-term validity requires subsequent clinical assessment).
+
+conf_dict["paths"]["ctx/composer"] = {
+    "maps_to": [],
+    "suffixes": {
+        "|name": "DataHub",
+        # "|id": "???",
+        # "|id_scheme": "???",
+        # "|id_namespace": "???"
+    }
+} 
+
+# conf_dict["paths"]["ctx/setting|code"] = ??
+# conf_dict["paths"]["ctx/setting|value"] = ??
+
+conf_dict["paths"]["ctx/encoding|code"] = "UTF-8"
+conf_dict["paths"]["ctx/language"] = "en"
+conf_dict["paths"]["ctx/territory"] = "nl"
+
+conf_dict["paths"]["ctx/start_time"] = datetime.now().isoformat()
+
+
 with open(conf_file, 'w') as outfile:
     yaml.dump(conf_dict, outfile, default_flow_style=False)
 
+with open(conf_file, 'r') as file:
+    print(file.read())
 
-print("STEP 3: Create a composition for a single patient")
+
+print("\nSTEP 3: Create a flat composition for a single patient")
 
 patient_id = "fc5b4365-670e-1d8b-b592-95a930c00c6c"
 input_file = SYNTHEA_PATH / f"{patient_id}.json"
-
 
 file = io.StringIO()
 with redirect_stdout(file):
@@ -102,19 +118,43 @@ with redirect_stdout(file):
         conf_file=conf_file,
         skip_ehr_id=True,
     )
-patient_flat_composition = file.getvalue()
-print("flat composition:")
-print(patient_flat_composition)
-with open(f"data/patient_flat_composition.json", "w") as file:
-    print(patient_flat_composition, file=file)
+patient_flat_composition = ast.literal_eval(file.getvalue())
+print(json.dumps(patient_flat_composition, indent = 4))
 # assert json.loads(stdout) == expected_composition
 
 
+print("\nSTEP 4: POST composition")
 
-print("STEP 4: Create composition")
+template_id = "patient"
+post_template(TEMPLATE_PATH / f"{template_id}.opt")
 
-post_template(TEMPLATE_PATH / "patient.opt")
 ehr_id = create_ehr(patient_id)
+# in composition.py, line "Accept": "application/json; charset=UTF-8" needed to be commented
+# response = post_flat_composition(ehr_id, template_id, patient_flat_composition)
 
-# need to convert flat composition to "normal" composition ..
-reponse = post_composition(ehr_id, patient_flat_composition)
+
+flat_composition = patient_flat_composition
+
+url = f"{ECIS_BASE_URL}/composition/?format=FLAT&ehrId={ehr_id}&templateId={template_id}"
+
+headers = {
+    # "Accept": "application/json; charset=UTF-8",
+    # "Prefer": "return=representation",
+    "Content-Type": "application/json",
+}
+response = requests.request(
+    "POST",
+    url,
+    headers=headers,
+    data=json.dumps(flat_composition),
+    auth=(EHRBASE_USERRNAME, EHRBASE_PASSWORD),
+    timeout=10,
+)
+
+response_json = json.loads(response.text)
+print(f"RESPONSE: {response.status_code}")
+if response.ok:
+    print(f"Composition was successfully created")
+else:
+    print(f'ERROR {response_json["error"]}')
+    print(response_json["message"])
