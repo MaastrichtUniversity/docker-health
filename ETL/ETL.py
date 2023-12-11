06c7d9eb1dd3fbb2fdd111aad67aea37bca3960d
hkabbech <helene.kabbech@gmail.com>
2023-12-11 16:32:23 +0100

    extract data for json patient format (WIP) [DHHDP-61, DHHDP-37]

--- ETL/ETL.py
+++ ETL/ETL.py
@@ -15,7 +15,7 @@ from src.composition import post_composition, transform_composition
 from src.diagnosis import parse_all_diagnosis
 from src.ehr import create_ehr, fetch_all_ehr_id
 from src.encounter import parse_all_encounters
-from src.patient import parse_patient
+from src.patient import parse_patient_csv, parse_patient_json, create_patient_attribute
 from src.template import fetch_all_templates, post_template
 from src.vitalsigns import  parse_vital_signs
 
@@ -38,16 +38,11 @@ def list_all_templates():
 def run():
     """Runs the ETL"""
 
+    INPUT_FORMAT = 'json' # to set as an argument
+
     TEMPLATE_PATH = Path("data/templates")
-    SYNTHEA_PATH = Path("data/synthea_csv")
+    SYNTHEA_PATH = Path(f"data/{input_format}")
 
-    # Load datasets
-    patients_df = pd.read_csv(SYNTHEA_PATH / "patients.csv")
-    conditions_df = pd.read_csv(SYNTHEA_PATH / "conditions.csv")
-    observations_df = pd.read_csv(SYNTHEA_PATH / "observations.csv")
-    encounters_df = pd.read_csv(SYNTHEA_PATH / "encounters.csv")
-
-    all_encounters = parse_all_encounters(encounters_df)
 
     print("\n\nSTEP 1 : POST templates")
     post_template(TEMPLATE_PATH / "vital_signs.opt")
@@ -56,14 +51,43 @@ def run():
     # fetch_all_templates()
 
     print("\n\nSTEP 2 : Create EHR for the first patient of the dataset")
-    patient_id = patients_df.iloc[0]["Id"]
+    # patient_id = patients_df.iloc[0]["Id"]
+    patient_id = "0a4a74f1-4444-6921-bf87-87fe209bec2e"
     ehr_id = create_ehr(patient_id)
     print(f"ehr_id: {ehr_id}")
 
-    print("\n\nSTEP 3 : Create compositions:")
 
-    print("\nPatient composition..")
-    patient = parse_patient(patients_df[patients_df["Id"] == patient_id])
+    print("\n\nSTEP 3 : Extract Data")
+
+
+    if input_format == 'csv':
+        # Load datasets
+        patients_df = pd.read_csv(SYNTHEA_PATH / "patients.csv")
+        conditions_df = pd.read_csv(SYNTHEA_PATH / "conditions.csv")
+        observations_df = pd.read_csv(SYNTHEA_PATH / "observations.csv")
+        encounters_df = pd.read_csv(SYNTHEA_PATH / "encounters.csv")
+
+        all_encounters = parse_all_encounters(encounters_df)
+
+        print("\nPatient composition..")
+        patient_raw_data = patients_df[patients_df["Id"] == patient_id]
+        patient = create_patient_attribute(*parse_patient_csv(patient_raw_data))
+
+
+    elif input_format == 'json':
+        # Load json patient file as a python dictionary
+        with open(SYNTHEA_PATH / f"{patient_id}.json") as infile:
+            patient_json = json.load(infile)
+
+        print("\nPatient composition..")
+        create_patient_attribute(*parse_patient_json(patient_json))
+
+
+
+
+
+    print("\n\nSTEP 4 : Transform and Load compositions")
+
     patient_json_str = patient.model_dump_json(by_alias=True, indent=4)
     print(f"\npatient: {patient_json_str}")
     patient_composition = transform_composition(patient.model_dump_json(by_alias=True), "patient")
@@ -72,53 +96,53 @@ def run():
     patient_composition_uuid = post_composition(ehr_id, patient_composition)
     print(f"\npatient_composition_uuid: {patient_composition_uuid}")
 
-    print("\nVital Signs compositions..")
-    vitalsigns_variables = [
-        {'name': 'Body Height', 'units': 'cm'},
-        # {'name': 'Body Weight', 'units': 'kg'},
-        # {'name': 'Heart rate', 'units': '/min'},
-        # {'name': 'Systolic Blood Pressure', 'units': 'mm[Hg]'},
-        # {'name': 'Diastolic Blood Pressure', 'units': 'mm[Hg]'}
-    ]
+        # print("\nVital Signs compositions..")
+        # vitalsigns_variables = [
+        #     {'name': 'Body Height', 'units': 'cm'},
+        #     # {'name': 'Body Weight', 'units': 'kg'},
+        #     # {'name': 'Heart rate', 'units': '/min'},
+        #     # {'name': 'Systolic Blood Pressure', 'units': 'mm[Hg]'},
+        #     # {'name': 'Diastolic Blood Pressure', 'units': 'mm[Hg]'}
+        # ]
 
-    patient_encounter_ids = [encounter_id for encounter_id, encounter in all_encounters.items() if
-                             encounter.patient_id == patient_id]
-    # patient_encounter_ids = encounters_df[encounters_df["PATIENT"] == patient_id]["Id"].tolist()
-    for encounter_id in patient_encounter_ids:
-        vitalsigns_df = observations_df[
-            (observations_df["ENCOUNTER"] == encounter_id) & \
-            (observations_df["CATEGORY"] == "vital-signs") & \
-            (observations_df["DESCRIPTION"].isin([v["name"] for v in vitalsigns_variables]))
-            ]
+        # patient_encounter_ids = [encounter_id for encounter_id, encounter in all_encounters.items() if
+        #                          encounter.patient_id == patient_id]
+        # # patient_encounter_ids = encounters_df[encounters_df["PATIENT"] == patient_id]["Id"].tolist()
+        # for encounter_id in patient_encounter_ids:
+        #     vitalsigns_df = observations_df[
+        #         (observations_df["ENCOUNTER"] == encounter_id) & \
+        #         (observations_df["CATEGORY"] == "vital-signs") & \
+        #         (observations_df["DESCRIPTION"].isin([v["name"] for v in vitalsigns_variables]))
+        #         ]
 
-        if vitalsigns_df.shape[0] == 0:
-            # print(f"Encounter id {encounter_id} has no vital signs observations")
-            continue
+        #     if vitalsigns_df.shape[0] == 0:
+        #         # print(f"Encounter id {encounter_id} has no vital signs observations")
+        #         continue
 
-        vitalsigns = parse_vital_signs(vitalsigns_df, vitalsigns_variables)
-        vitalsigns_json_str = vitalsigns.model_dump_json(by_alias=True, indent=4)
-        print(f"\ndiagnosis: {vitalsigns_json_str}")
-        vitalsigns_composition = transform_composition(vitalsigns.model_dump_json(by_alias=True), "vital_signs")
-        # print(f"\ncomposition: {vitalsigns_composition}")
+        #     vitalsigns = parse_vital_signs(vitalsigns_df, vitalsigns_variables)
+        #     vitalsigns_json_str = vitalsigns.model_dump_json(by_alias=True, indent=4)
+        #     print(f"\ndiagnosis: {vitalsigns_json_str}")
+        #     vitalsigns_composition = transform_composition(vitalsigns.model_dump_json(by_alias=True), "vital_signs")
+        #     # print(f"\ncomposition: {vitalsigns_composition}")
 
-        vitalsigns_composition_uuid = post_composition(ehr_id, vitalsigns_composition)
-        print(f"\ndiagnosis_composition_uuid: {vitalsigns_composition_uuid}")
+        #     vitalsigns_composition_uuid = post_composition(ehr_id, vitalsigns_composition)
+        #     print(f"\ndiagnosis_composition_uuid: {vitalsigns_composition_uuid}")
 
-    # plot_bloodpressure_over_time(ehr_id)
+        # # plot_bloodpressure_over_time(ehr_id)
 
-    print("\nDiagnosis compositions..")
-    where_disorder = conditions_df.DESCRIPTION.apply(lambda x: bool(re.search('.*(disorder)', x)))
-    conditions_df = conditions_df[where_disorder]
-    patient_diagnosis_df = conditions_df[conditions_df["PATIENT"] == patient_id]
-    for _, diagnosis_df in patient_diagnosis_df.iterrows():
-        diagnosis = parse_all_diagnosis(diagnosis_df)
-        diagnosis_json_str = diagnosis.model_dump_json(by_alias=True, indent=4)
-        print(f"\ndiagnosis: {diagnosis_json_str}")
-        diagnosis_composition = transform_composition(diagnosis.model_dump_json(by_alias=True), "diagnosis-demo")
-        # print(f"\ncomposition: {diagnosis_composition}")
+        # print("\nDiagnosis compositions..")
+        # where_disorder = conditions_df.DESCRIPTION.apply(lambda x: bool(re.search('.*(disorder)', x)))
+        # conditions_df = conditions_df[where_disorder]
+        # patient_diagnosis_df = conditions_df[conditions_df["PATIENT"] == patient_id]
+        # for _, diagnosis_df in patient_diagnosis_df.iterrows():
+        #     diagnosis = parse_all_diagnosis(diagnosis_df)
+        #     diagnosis_json_str = diagnosis.model_dump_json(by_alias=True, indent=4)
+        #     print(f"\ndiagnosis: {diagnosis_json_str}")
+        #     diagnosis_composition = transform_composition(diagnosis.model_dump_json(by_alias=True), "diagnosis-demo")
+        #     # print(f"\ncomposition: {diagnosis_composition}")
 
-        diagnosis_composition_uuid = post_composition(ehr_id, diagnosis_composition)
-        print(f"\ndiagnosis_composition_uuid: {diagnosis_composition_uuid}")
+        #     diagnosis_composition_uuid = post_composition(ehr_id, diagnosis_composition)
+        #     print(f"\ndiagnosis_composition_uuid: {diagnosis_composition_uuid}")
 
 
 @click.group()
--- ETL/src/patient.py
+++ ETL/src/patient.py
@@ -20,7 +20,31 @@ class Patient(BaseModel):
     start_time: datetime = Field(default_factory=datetime_now, serialization_alias="startTime")
 
 
-def parse_patient(patient_df: pd.DataFrame) -> Patient:
+def create_patient_attribute(gender_code, birth_date, death_date) -> Patient:
+    """
+    check format (ISO and local terms) and create a Patient attribute
+    TO DO
+    """
+    if gender_code not in ["M", "F", "I"]:  # code for Male, Female, Intersec
+        gender_code = None
+
+    try:
+        birth_date = datetime.fromisoformat(birth_date).isoformat()
+    except TypeError:
+        birth_date = None
+
+    try:
+        death_date = datetime.fromisoformat(death_date).isoformat()
+    except TypeError:
+        death_date = None
+
+    if death_date is None:
+        return Patient(gender_code=gender_code, birth_date=birth_date)
+
+    return Patient(gender_code=gender_code, birth_date=birth_date, death_date=death_date)
+
+
+def parse_patient_csv(patient_df: pd.DataFrame):
     """
     Parse a unique patient dataframe to a patient class
     Parameters
@@ -40,19 +64,41 @@ def parse_patient(patient_df: pd.DataFrame) -> Patient:
     patient_df = patient_df.squeeze()
 
     # Parse Gender (Sex assigned at birth):
-    gender = patient_df["GENDER"]
-    if gender not in ["M", "F", "I"]:  # code for Male, Female, Intersec
-        gender = None
+    if "GENDER" in patient_df:
+        gender_code = patient_df["GENDER"]
+    else:
+        gender_code = None
 
-    # Parse birthdate:
-    try:
-        birthdate = datetime.fromisoformat(patient_df["BIRTHDATE"]).isoformat()
-    except TypeError:
-        birthdate = None
+    if "BIRTHDATE" in patient_df:
+        birth_date = patient_df["BIRTHDATE"]
+    else:
+        birth_date = None
 
-    # Parse deathdate:
-    try:
-        death_date = datetime.fromisoformat(str(patient_df["DEATHDATE"]))  # str for None values
-        return Patient(gender=gender, birthdate=birthdate, deathdate=death_date)
-    except ValueError:
-        return Patient(gender=gender, birthdate=birthdate)
+    if "DEATHDATE" in patient_df:
+        death_date = patient_df["DEATHDATE"]
+    else:
+        death_date = None
+
+    return gender_code, birth_date, death_date
+
+
+def parse_patient_json(patient_json: dict):
+    """
+    TO DO
+    """
+    if "gender" in patient_json["attributes"]:
+        gender_code = patient_json["attributes"]["gender"]
+    else:
+        gender_code = None
+
+    if "birthdate_as_localdate" in patient_json["attributes"]:
+        birth_date = patient_json["attributes"]["birthdate_as_localdate"]
+    else:
+        birth_date = None
+
+    if "deathdate" in patient_json["attributes"]:
+        death_date = patient_json["attributes"]["deathdate"]
+    else:
+        death_date = None
+
+    return gender_code, birth_date, death_date
