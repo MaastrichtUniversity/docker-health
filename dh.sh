c725d5f6a2a51823b969e9db7e1b9211a52946b3
jonathanMELIUS <meliusjonathan@gmail.com>
2025-11-18 16:22:11 +0100

    Add elk host in dh.sh [DHHDP-1343, DHHDP-1407]

--- dh.sh
+++ dh.sh
@@ -211,6 +211,7 @@ setup_hosts() {
 #!/bin/bash
 MINIKUBE_IP=$(minikube ip)
 
+echo "$MINIKUBE_IP elk.local.dh.unimaas.nl" | sudo tee -a /etc/hosts
 echo "$MINIKUBE_IP transform.local.dh.unimaas.nl" | sudo tee -a /etc/hosts
 echo "$MINIKUBE_IP federation.local.dh.unimaas.nl" | sudo tee -a /etc/hosts
 echo "$MINIKUBE_IP jupyter.local.dh.unimaas.nl" | sudo tee -a /etc/hosts
