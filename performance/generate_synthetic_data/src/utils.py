a3a3942eacedc496eeecdcd84677a8906ddfb1f9
hkabbech <helene.kabbech@gmail.com>
2026-04-14 14:18:09 +0200

    Import randint and choice function from numpy.random [DHHDP-1713, DHHDP-1757]

--- performance/generate_synthetic_data/src/utils.py
+++ performance/generate_synthetic_data/src/utils.py
@@ -1,13 +1,10 @@
-import random
 import string
-import numpy as np
 from enum import StrEnum
 from datetime import datetime, timedelta
 from faker import Faker
+from numpy.random import seed, randint, choice
 
-np.random.seed(42)
-random.seed(42)
-
+seed(42)
 Faker.seed(42)
 FAKE = Faker("nl_NL")
 
@@ -24,25 +21,25 @@ def generate_bsn(used_bsn):
 
 
 def optional(value, p):
-    return np.random.choice([value, None], p=[p, 1 - p])
+    return choice([value, None], p=[p, 1 - p])
 
 
 def generate_random_full_date(start_date=datetime(2020, 1, 1), end_date=datetime(2026, 12, 31)):
     delta = end_date - start_date
-    random_days = random.randint(0, delta.days)
-    random_seconds = random.randint(0, 86399)  # Seconds in a day
+    random_days = randint(0, delta.days)
+    random_seconds = randint(0, 86399)  # Seconds in a day
     date = start_date + timedelta(days=random_days, seconds=random_seconds)
     return date.strftime("%Y-%m-%dT%H:%M:%S")
 
 
 def generate_random_date(start_date=datetime(2020, 1, 1), end_date=datetime(2026, 12, 31), p_partial=[0.7, 0.2, 0.1]):
     delta = end_date - start_date
-    random_days = random.randint(0, delta.days)
+    random_days = randint(0, delta.days)
     date = start_date + timedelta(days=random_days)
-    choice = np.random.choice(["yyyy-mm-dd", "yyyy-mm", "yyyy"], p=p_partial)
-    if choice == "yyyy-mm-dd":
+    format = choice(["yyyy-mm-dd", "yyyy-mm", "yyyy"], p=p_partial)
+    if format == "yyyy-mm-dd":
         return date.strftime("%Y-%m-%d")
-    elif choice == "yyyy-mm":
+    elif format == "yyyy-mm":
         return date.strftime("%Y-%m")
     else:
         return date.strftime("%Y")
@@ -50,10 +47,10 @@ def generate_random_date(start_date=datetime(2020, 1, 1), end_date=datetime(2026
 
 def generate_random_string(min_len=5, max_len=50):
     chars = list(string.ascii_lowercase)
-    length = np.random.randint(min_len, max_len + 1)
-    random_chars = np.random.choice(chars, size=length)
+    length = randint(min_len, max_len + 1)
+    random_chars = choice(chars, size=length)
     return "".join(random_chars)
 
 
 def generate_random_coded_value(coded_list):
-    return str(np.random.choice(coded_list))
+    return str(choice(coded_list))
